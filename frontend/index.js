e9b429b1d551fd89137b30264eea1a33846c0dbf
Johaney-s <johana.supikova@seznam.cz>
2024-10-17 11:55:21 +0200

    switch to odata
    * use odata as source of features

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,5 @@
 const backendHost = 'http://127.0.0.1:8000';
-const apiRoot = "https://catalogue.dataspace.copernicus.eu";
+const apiRoot = "https://catalogue.dataspace.copernicus.eu/odata/v1/Products";
 const supportEmail = "placeholder@example.com"; //TODO Change email
 
 let leafletMap = L.map('mapDiv').setView([50.05, 14.46], 10);
@@ -26,14 +26,14 @@ timeTo.setUTCSeconds(59);
 let timeToInput = document.querySelector("#timeToInput");
 timeToInput.value = timeTo.toISOString().substring(0, 16);
 
-const offeredDatasets = [
-    "SENTINEL-1",
-    "SENTINEL-2",
-    "SENTINEL-3",
-    "SENTINEL-5P",
-    "SENTINEL-6",
-    "SENTINEL-1-RTC",
-]
+// const offeredDatasets = [
+//     "SENTINEL-1",
+//     "SENTINEL-2",
+//     "SENTINEL-3",
+//     "SENTINEL-5P",
+//     "SENTINEL-6",
+//     "SENTINEL-1-RTC",
+// ]
 
 const prepareDatasetSelect = () => {
     /*
@@ -59,13 +59,13 @@ const prepareDatasetSelect = () => {
             ]
      */
 
-    let datasetsSelect = document.getElementById('datasetsSelect');
-    for (let dataset of offeredDatasets) {
-        let option = document.createElement("option");
-        option.value = dataset;
-        option.innerHTML = dataset;
-        datasetsSelect.appendChild(option);
-    }
+    // let datasetsSelect = document.getElementById('datasetsSelect');
+    // for (let dataset of offeredDatasets) {
+    //     let option = document.createElement("option");
+    //     option.value = dataset;
+    //     option.innerHTML = dataset;
+    //     datasetsSelect.appendChild(option);
+    // }
 }
 
 
@@ -131,18 +131,16 @@ const fetchFeaturesFromCopernicus = async (endpoint) => {
         try {
             const response = await fetch(endpoint);
             const data = await response.json();
+            console.log(data);
 
-            features = features.concat(data.features);
+            features = features.concat(data.value);
 
-            for (const link of data.links) {
-                if (link.rel === "next") {
-                    endpoint = link.href;
-                    foundNextPage = true;
-                    break;
-                }
+            if (! '@odata.nextLink' in data) {
+                break;
             }
 
-            if (!foundNextPage) {
+            if (features.length >= 100) {
+                // let's not load the whole collection
                 break;
             }
 
@@ -263,30 +261,36 @@ const fetchFeatures = async () => {
             coordinatesUserInput[1][1]
         );
 
-        let dataset = document.querySelector("#datasetsSelect").value;
+        const selectedDatasources = document.querySelectorAll('input[name="dataset"]:checked');
+        const datasetValues = Array.from(selectedDatasources).map(checkbox => checkbox.value);
+        console.log(datasetValues);
 
-        let endpoint = new URL(["stac", "collections", dataset, "items"].join("/"), apiRoot);
-        endpoint.searchParams.set("bbox", bbox);
-        endpoint.searchParams.set("datetime", [timeFrom.toISOString(), timeTo.toISOString()].join("/"));
-        endpoint.searchParams.set("limit", "100");
 
-        let obtainedFeatures = await fetchFeaturesFromCopernicus(endpoint.href);
+        // let endpoint = new URL(["stac", "collections", dataset, "items"].join(""), apiRoot);
+        // endpoint.searchParams.set("bbox", bbox);
+        // endpoint.searchParams.set("datetime", [timeFrom.toISOString(), timeTo.toISOString()].join("/"));
+        // endpoint.searchParams.set("limit", "100");
 
-        console.log(obtainedFeatures)
+        let filterCondition = datasetValues
+            .map(source => `Collection/Name eq '${source}'`)
+            .join(' or ');
+        let endpoint = `${apiRoot}?$filter=${filterCondition}`;
+        let obtainedFeatures = await fetchFeaturesFromCopernicus(endpoint);
+
 
         let availableFeaturesSelect = document.querySelector("#availableFeaturesSelect");
         availableFeaturesSelect.innerHTML = '';
 
-        obtainedFeatures.sort((a, b) => a.id.toLowerCase().localeCompare(b.id.toLowerCase()));
+        obtainedFeatures.sort((a, b) => a.Id.toLowerCase().localeCompare(b.Id.toLowerCase()));
 
         features = new Map();
 
         for (const feature of obtainedFeatures) {
-            features.set(feature.id, feature);
+            features.set(feature.Id, feature);
 
             let option = document.createElement("option");
-            option.value = feature.id;
-            option.textContent = feature.id;
+            option.value = feature.Name;
+            option.textContent = feature.Name;
 
             availableFeaturesSelect.appendChild(option);
         }
