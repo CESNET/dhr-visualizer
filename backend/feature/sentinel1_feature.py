7a9d607415d7ab2e8fc2b2d2465665762d87f35f
Jiří Matějka <jirimatejka@cesnet.cz>
2025-04-03 18:05:45 +0200

    sentinel1 filtering, wip

--- backend/feature/sentinel1_feature.py
+++ backend/feature/sentinel1_feature.py
@@ -54,13 +54,13 @@ class Sentinel1Feature(RequestedFeature):
 
         for available_file in available_files:
             if not (
-                    re.search('/measurement/', available_file)
+                    re.search('/measurement/', available_file[0].strip())
             ):
                 continue
 
             for polarisation_channel in polarisation_filter:
-                if re.search(f'.+-{polarisation_channel}-.+', available_file):
-                    if available_file.split('.')[-1].lower() in ['tif', 'tiff']:
+                if re.search(f'.+-{polarisation_channel}-.+', available_file[0].strip()):
+                    if available_file[0].split('.')[-1].lower() in ['tif', 'tiff']:
                         self._filters_polarisation_channels_availability[polarisation_channel.upper()] = True
                         filtered_files.append(available_file)
                         break
