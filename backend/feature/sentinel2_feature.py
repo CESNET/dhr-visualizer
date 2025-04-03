da42cf3536d016f70789f23ce5788a5b29c86b2d
Jiří Matějka <jirimatejka@cesnet.cz>
2025-04-03 13:17:12 +0200

    Updated regex pattern

--- backend/feature/sentinel2_feature.py
+++ backend/feature/sentinel2_feature.py
@@ -29,10 +29,10 @@ class Sentinel2Feature(RequestedFeature):
         selected_bands_pattern = "|".join(self._get_selected_bands())
         extensions = ['jp2', 'j2k', 'jpf', 'jpm', 'jpg2', 'j2c', 'jpc', 'jpx', 'mj2']
         extensions_pattern = "|".join(extensions)
-        regex_patern = rf"([^/]+)/GRANULE/([^/]+)/IMG_DATA/([^/]+_({selected_bands_pattern})\.({extensions_pattern}))"
+        regex_pattern = rf"([^/]+)/GRANULE/([^/]+)/IMG_DATA/(?:R\d{{2}}m/)?([^/]+_({selected_bands_pattern})(?:_\d{{2}}m)?\.({extensions_pattern}))"
 
         for available_file in available_files:
-            if re.match(regex_patern, available_file[0].strip()):
+            if re.match(regex_pattern, available_file[0].strip()):
                 filtered_files.append(available_file)
 
         return filtered_files
