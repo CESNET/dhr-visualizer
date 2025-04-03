1b692a8a5a06639ea0e06b1f1782a49a14a4fac4
Jiří Matějka <jirimatejka@cesnet.cz>
2025-04-03 16:34:34 +0200

    gjtiff docker execution

--- backend/feature/requested_feature.py
+++ backend/feature/requested_feature.py
@@ -153,17 +153,23 @@ class RequestedFeature(ABC):
         cmd = f"docker exec gjtiff_container gjtiff -q 82 -o {str(self._output_directory)} {file_list}"
         print("GENERATING MAP TILES 02")
         gjtiff_stdout = self._run_gjtiff_docker(input_files=input_files, output_directory=self._output_directory)
-        processed_tiles = self._extract_output_file_list(stdout=gjtiff_stdout)
         print("GENERATING MAP TILES 03")
-        print(f"PROCESSEDTILES_START >>> {processed_tiles} <<< PROCESSEDTILES_END")
+        print(f"GJTIFFSTDOUT_START>>>{gjtiff_stdout}<<<GJTIFFSTDOUT_END")
         print("GENERATING MAP TILES 04")
+        processed_tiles = self._extract_output_file_list(stdout=gjtiff_stdout)
+        print("GENERATING MAP TILES 05")
+        print(f"PROCESSEDTILES_START >>> {processed_tiles} <<< PROCESSEDTILES_END")
+        print("GENERATING MAP TILES 06")
 
         return processed_tiles
 
     def _extract_output_file_list(self, stdout: str) -> list[str] | None:
+        print(f"EXTRACT_STDOUT_START>>>{stdout}<<<EXTRACT_STDOUT_END")
         json_list_pattern = r'\[.*\]'
         matches = re.findall(json_list_pattern, stdout)
+        print(f"MATCHES_START>>>{matches}<<<MATCHES_END")
         last_json_list = matches[-1] if matches else None
+        print(f"LASTJSONLIST_START>>>{last_json_list}<<<LASTJSONLIST_END")
         return json.loads(last_json_list)
 
     def _run_gjtiff_docker(self, input_files: list[str] = None, output_directory: Path = _output_directory) -> str:
