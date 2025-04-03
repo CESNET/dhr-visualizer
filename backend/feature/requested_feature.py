aa2321110cc1d4e29b7a5d7b390dd8420e5e041d
Jiří Matějka <jirimatejka@cesnet.cz>
2025-04-03 14:48:41 +0200

    gjtiff docker execution

--- backend/feature/requested_feature.py
+++ backend/feature/requested_feature.py
@@ -134,7 +134,7 @@ class RequestedFeature(ABC):
         self._set_status(status=RequestStatuses.COMPLETED)
 
     def _generate_map_tiles(self, input_files: list[str]) -> list[str] | None:
-        file_list = ' '.join(input_files)
+        file_list = ','.join(input_files)
 
         # self._output_directory = Path(f"{variables.FRONTEND_WEBSERVER_ROOT_DIR}/output/{self._request_hash}")
         self._output_directory = Path(f"/data/output/{self._request_hash}")
