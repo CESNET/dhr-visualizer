44c3a826bd4bb2290ce848c22b8b39d2fa9ae927
Jiří Matějka <jirimatejka@cesnet.cz>
2025-04-08 11:28:31 +0200

    var

--- backend/config/variables.py
+++ backend/config/variables.py
@@ -1,6 +1,6 @@
 import os
 
-DHR__USE_DHR: bool = os.environ.get("DHR__USE_DHR").lower() in ["1", "true", "yes", ]
+DHR__USE_DHR: bool = os.environ.get("DHR__USE_DHR", default="False").lower() in ["1", "true", "yes", ]
 DHR__CATALOG_ROOT: str = os.getenv("DHR__CATALOG_ROOT")
 
 CDSE__CATALOG_ROOT: str = os.getenv("CDSE__CATALOG_ROOT")
