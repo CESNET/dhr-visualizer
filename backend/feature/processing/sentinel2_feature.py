f400616299a394214b2296e28c9ddc84a5358ac0
Johaney-s <johana.supikova@seznam.cz>
2025-07-16 16:49:34 +0200

    Prune duplicate image resolutions
    * keep only the best resolution from each S2 product band
    * will affect S2 products containing resolution in image names

--- backend/feature/processing/sentinel2_feature.py
+++ backend/feature/processing/sentinel2_feature.py
@@ -37,6 +37,8 @@ class Sentinel2Feature(ProcessedFeature):
             if re.match(regex_pattern, available_file[0].strip()):
                 filtered_files.append(available_file)
 
+        filtered_files = self._prune_low_resolution_files(filtered_files)
+
         return filtered_files
 
     def _get_selected_bands(self):
@@ -50,3 +52,32 @@ class Sentinel2Feature(ProcessedFeature):
                 selected_bands.append(f'B{int(band[1:]):02}')
 
         return selected_bands
+
+    def _prune_low_resolution_files(self, files: list[tuple[str, str]]):
+        """
+        Keep only files with the highest resolution in the list of files.
+        We can spot such behaviour in the name such as /some/path/STH_DATE_B02_10m.jp2.
+        """
+        if len(files) == 0:
+            return files
+
+        best_resolution = {}  # {"B02": (resolution, index)}
+
+        for i in range(len(files)):
+            filename = files[i][0].split("/")[-1]
+            filename_parts = re.split(r'[_.]', filename)
+
+            if len(filename_parts) != 5:
+                return files  # these files do not contain resolution
+
+            band = filename_parts[2]
+            resolution = filename_parts[3].replace("m", "")
+
+            if band not in best_resolution:
+                best_resolution[band] = (resolution, i)
+                continue
+
+            if int(resolution) < int(best_resolution[band][0]):
+                best_resolution[band] = (resolution, i)
+
+        return [files[i] for r,i in best_resolution.values()]
