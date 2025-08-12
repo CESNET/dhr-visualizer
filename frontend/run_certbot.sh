b6a69b1c3488448cce868f615be103257b1cbb97
Jiří Matějka <jirimatejka@cesnet.cz>
2025-08-12 09:45:36 +0200

    Apache certificate handling

---
+++ frontend/run_certbot.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+CERT_DIR="/etc/letsencrypt"
+WEBROOT="/var/www/certbot"
+
+if [ ! -f "$CERT_DIR/live/$DOMAIN/fullchain.pem" ]; then
+  # HTTPS certificate does not exist
+  echo "Requesting new HTTPS certificate"
+  certbot certonly --webroot -w $WEBROOT -d $FRONTEND_DOMAIN --non-interactive --agree-tos --email $FRONTEND_EMAIL
+else
+  # HTTPS certificate exists
+  echo "HTTPS certificate exist, attempting renewal"
+  certbot renew --webroot -w $WEBROOT --non-interactive --quiet
+fi
+
+# Renew certificate every 12 hours
+while true; do
+  sleep 43200  # 43200 seconds == 12 hours
+  certbot renew --webroot -w $WEBROOT --non-interactive --quiet
+done
