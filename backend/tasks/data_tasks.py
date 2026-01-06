0488dd4f875d97861afbefcb0970e837fcaa259d
Johaney-s <johana.supikova@seznam.cz>
2026-01-06 14:59:23 +0100

    Delete feature files after one hour

--- backend/tasks/data_tasks.py
+++ backend/tasks/data_tasks.py
@@ -1,3 +1,7 @@
+import shutil
+from pathlib import Path
+
+import variables
 from celery_app import celery_app
 from database.mongo_database_connector import MongoDatabaseConnector
 from celery.utils.log import get_task_logger
@@ -24,6 +28,7 @@ def download_feature_task(feature_id: str):
     _db.set(key=feature_id, value=feature)
     if feature.get_status() == RequestStatuses.PROCESSING:
         process_feature_task.delay(feature_id)
+        delete_feature_task.apply_async(args=[feature_id], countdown=60*60)
 
 @celery_app.task(ignore_result=True)
 def process_feature_task(feature_id: str):
@@ -33,5 +38,13 @@ def process_feature_task(feature_id: str):
 
     feature.process_feature()
     _db.set(key=feature_id, value=feature)
-
-# todo - add cleaning task for downloaded files? for processed files?
+    
+@celery_app.task(ignore_result=True)
+def delete_feature_task(feature_id: str):
+    """ Removes all downloaded and processed files related to the feature"""
+    init_db()
+    logger.info(f"Delete task for {feature_id}")
+    _db.delete(feature_id)
+    # todo - handle repetitive requests (e.g. newest product) - make last_accesed field in DB and check by it?
+    feature_dir = Path(variables.DOCKER_SHARED_DATA_DIRECTORY, feature_id)
+    shutil.rmtree(feature_dir)
